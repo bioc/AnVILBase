bd78ba95160fb3ac1a6c7e35d9dbf75b29c57687
LiNk-NY <marcel.ramos@roswellpark.org>
2024-03-19 14:30:18 -0400

    add avnotebooks generics
    - includes *_(de)localize

---
+++ R/avnotebooks-generics.R
@@ -0,0 +1,44 @@
+#' @name avnotebooks-generics
+#'
+#' @aliases avnotebooks avnotebooks_localize avnotebooks_delocalize
+#'
+#' @title All the avnotebooks generics for AnVIL
+#'
+#' @description These are the generics to be used by the AnVIL package.
+#'
+#' @inheritParams AnVILBase-generics
+#'
+#' @seealso [AnVILBase-generics], [avworkspace-generics], [avtable-generics],
+#'  [avdata-generics], [avworkflow-generics]
+#'
+NULL
+
+# avnotebooks --------------------------------------------------------------
+
+#' @rdname avnotebooks-generics
+#' @export
+setGeneric(
+    "avnotebooks",
+    function(..., platform) standardGeneric("avnotebooks"),
+    signature = "platform"
+)
+
+# avnotebooks_localize -----------------------------------------------------
+
+#' @rdname avnotebooks-generics
+#' @export
+setGeneric(
+    "avnotebooks_localize",
+    function(..., platform) standardGeneric("avnotebooks_localize"),
+    signature = "platform"
+)
+
+# avnotebooks_delocalize ---------------------------------------------------
+
+#' @rdname avnotebooks-generics
+#' @export
+setGeneric(
+    "avnotebooks_delocalize",
+    function(..., platform) standardGeneric("avnotebooks_delocalize"),
+    signature = "platform"
+)
---
+++ R/avnotebooks-methods.R
@@ -0,0 +1,77 @@
+#' @name avnotebooks-methods
+#'
+#' @title All the notebook type methods for AnVIL
+#'
+#' @description These are the methods to be used by the AnVIL package.
+#'
+#' @inheritParams avnotebooks-generics
+#'
+#' @include avnotebooks-generics.R
+#'
+NULL
+
+#' @describeIn avnotebooks-methods List the available notebooks in the current
+#'   workspace
+#'
+#' @exportMethod avnotebooks
+setMethod("avnotebooks", c(platform = "missing"),
+    function(..., platform) {
+        avnotebooks(..., platform = cloud_platform())
+    }
+)
+
+#' @describeIn avnotebooks-methods List the available notebooks in the current
+#'   workspace
+#'
+#' @exportMethod avnotebooks
+setMethod("avnotebooks", c(platform = "ANY"),
+    function(..., platform) {
+        stop("'avnotebooks()' not implemented for class ", class(platform))
+    }
+)
+
+#' @describeIn avnotebooks-methods sync the contents of the workspace storage to
+#'   the local file system
+#'
+#' @exportMethod avnotebooks_localize
+setMethod("avnotebooks_localize", c(platform = "missing"),
+    function(..., platform) {
+        avnotebooks_localize(..., platform = cloud_platform())
+    }
+)
+
+#' @describeIn avnotebooks-methods sync the contents of the workspace storage to
+#'   the local file system
+#'
+#' @exportMethod avnotebooks_localize
+setMethod("avnotebooks_localize", c(platform = "ANY"),
+    function(..., platform) {
+        stop(
+            "'avnotebooks_localize()' not implemented for class ",
+            class(platform)
+        )
+    }
+)
+
+#' @describeIn avnotebooks-methods sync the contents of the local file system to
+#'   the workspace storage
+#'
+#' @exportMethod avnotebooks_delocalize
+setMethod("avnotebooks_delocalize", c(platform = "missing"),
+    function(..., platform) {
+        avnotebooks_delocalize(..., platform = cloud_platform())
+    }
+)
+
+#' @describeIn avnotebooks-methods sync the contents of the local file system to
+#'   the workspace storage
+#'
+#' @exportMethod avnotebooks_delocalize
+setMethod("avnotebooks_delocalize", c(platform = "ANY"),
+    function(..., platform) {
+        stop(
+            "'avnotebooks_delocalize()' not implemented for class ",
+            class(platform)
+        )
+    }
+)
